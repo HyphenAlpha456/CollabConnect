22cb9830d0fcd9b52df072c395b9611eb06d16ad
HyphenAlpha456 <158492301+HyphenAlpha456@users.noreply.github.com>
2025-07-04 16:01:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ if __name__ == '__main__':
         save_json([], 'data/ideas.json')
     if not os.path.exists('data/forum.json'):
         save_json([], 'data/forum.json')
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0", port=port)
